bc6cdd98bd130228060291e02f33eada8ee3cf6c
glen <glenwojnar@yahoo.com>
2022-10-20 16:26:30 -0400

    solved reverse string

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,12 @@
 // Your code here
+const reverseString = require("../problems/reverse-string")
+const chai = require("chai")
+const expect = chai.expect
+
+describe("reverseString", () => {
+    it("should return string reversed", () => {
+        expect(reverseString("fun")).to.equal("nuf")
+    })
+})
+
+
